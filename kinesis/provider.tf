1879c6f6ed73a09930a9913628a668b7f09f2dff
chanaraman97 <66430954+chanaraman97@users.noreply.github.com>
2021-02-11 20:37:26 +0000

    Update provider.tf

--- kinesis/provider.tf
+++ kinesis/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region  = "ap-northeast-1"
-  access_key = "AKIAJOVJ3XWBXADNHUEA"
-  secret_key = "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"
+  region  = ""
+  access_key = ""
+  secret_key = ""
 }
