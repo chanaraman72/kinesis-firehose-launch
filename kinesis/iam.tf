13c080bed75278ad74bbfc5f4856f95307d79782
chanaraman97 <66430954+chanaraman97@users.noreply.github.com>
2021-02-11 14:18:14 +0000

    Add files via upload

---
+++ kinesis/iam.tf
@@ -0,0 +1,48 @@
+resource "aws_iam_role" "firehose-stream-role" {
+  //
+  name = "firehose-stream-role"
+  // role = "aws_iam_role.firehose-stream-role.id"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "firehose.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+}
+EOF
+}
+
+
+resource "aws_iam_role_policy" "firehose-stream-policy" {
+  name = "firehose-stream-policy"
+  role = "${aws_iam_role.firehose-stream-role.id}"
+
+ policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+       {
+            "Effect": "Allow",
+            "Action": "kinesis:*",
+            "Resource": "*"
+        },
+	    {
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": [
+              "arn:aws:s3:::s3-new-buck-1",
+              "arn:aws:s3:::s3-new-buck-1/*"
+            ]
+      }
+    ]
+}
+EOF
+}
---
+++ kinesis/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region  = "ap-northeast-1"
+  access_key = "AKIAJOVJ3XWBXADNHUEA"
+  secret_key = "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"
+}
